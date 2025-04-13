7351ae0e09db3c1ade7dc5bd4b192bfd61d7df35
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-04-13 15:13:15 +0900

    TodoTaskクラスにバリデーション属性を追加

--- TodoApp/Models/TOdoTask.cs
+++ TodoApp/Models/TOdoTask.cs
@@ -15,15 +15,15 @@ namespace TodoApp.Models
         /// <summary>
         /// タスクのタイトル
         /// </summary>
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "タイトルは必須です")]
+        [MaxLength(100, ErrorMessage = "タイトルは100文字以内で入力してください")]
         public string Title { get; set; } = string.Empty;
 
         /// <summary>
         /// タスクの詳細または説明
         /// </summary>
-        [Required]
-        [MaxLength(1000)]
+        [Required(ErrorMessage = "本文は必須です")]
+        [MaxLength(1000, ErrorMessage = "本文は1000文字以内で入力してください")]
         public string Body { get; set; } = string.Empty;
     }
 }
