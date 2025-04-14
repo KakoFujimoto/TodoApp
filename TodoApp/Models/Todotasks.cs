e0014cd40697230ef88e91fab410684ecba5122d
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-04-14 13:07:26 +0900

    Taskの一覧表示時に改行が反映されるように

--- TodoApp/Models/Todotasks.cs
+++ TodoApp/Models/Todotasks.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace TodoApp.Models
 {
@@ -25,5 +26,18 @@ namespace TodoApp.Models
         [Required(ErrorMessage = "本文は必須です")]
         [MaxLength(1000, ErrorMessage = "本文は1000文字以内で入力してください")]
         public string Body { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 安全に改行をHTMLとして表示するためのプロパティ
+        /// </summary>
+        public string FormattedBody
+        {
+            get
+            {
+                var encoded = WebUtility.HtmlEncode(Body ?? "");
+                return encoded.Replace("\r\n", "<br>").Replace("\n","<br>");
+            }
+        }
+
     }
 }
