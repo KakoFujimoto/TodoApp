b4827e2967625b97a7c39a89435f6ec3ed383f6a
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-04-14 18:15:41 +0900

    Revert "Taskの一覧表示時に改行が反映されるように"
    This reverts commit e0014cd40697230ef88e91fab410684ecba5122d.

--- TodoApp/Models/TOdoTask.cs
+++ TodoApp/Models/TOdoTask.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Net;
 
 namespace TodoApp.Models
 {
@@ -26,18 +25,5 @@ namespace TodoApp.Models
         [Required(ErrorMessage = "本文は必須です")]
         [MaxLength(1000, ErrorMessage = "本文は1000文字以内で入力してください")]
         public string Body { get; set; } = string.Empty;
-
-        /// <summary>
-        /// 安全に改行をHTMLとして表示するためのプロパティ
-        /// </summary>
-        public string FormattedBody
-        {
-            get
-            {
-                var encoded = WebUtility.HtmlEncode(Body ?? "");
-                return encoded.Replace("\r\n", "<br>").Replace("\n","<br>");
-            }
-        }
-
     }
 }
