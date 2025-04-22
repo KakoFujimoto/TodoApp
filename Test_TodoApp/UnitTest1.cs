7a785061d80a526d0452fe6254ec67e1a9f7953d
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-04-22 17:58:40 +0900

    テストプロジェクトの追加

---
+++ Test_TodoApp/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace Test_TodoApp;
+
+public class UnitTest1
+{
+    private static AppDbContext db;
+
+    public static void Initialize()
+    {
+        if (db != null) { return; }
+
+        var builder = new DbContextOptionsBuilder<AppDbContext>();
+        builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoAppDb-test;Trusted_Connection=True;");
+        db = new AppDbContext(builder.Options);
+
+        db.Database.EnsureDeleted();
+        db.Database.EnsureCreated();
+    }
+
+    public UnitTest1()
+    {
+        Initialize();
+    }
+
+    public static AppDbContext GetContext() => db;
+
+    [Fact]
+    public void Test1()
+    {
+        db.Add(TodoTask.Create("aa","sss"));
+        db.SaveChanges();
+
+        var item = db.TodoTasks.First();
+        Assert.Equal("aa", item.Title);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        db.Add(TodoTask.Create("bb","sss"));
+        db.SaveChanges();
+
+        var item = db.TodoTasks.First();
+        Assert.Equal("bb", item.Title);
+    }
+}
