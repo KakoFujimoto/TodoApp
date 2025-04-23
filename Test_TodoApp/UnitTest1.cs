c5a598f944bb4739d5e00cd1c3755800df582eef
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-04-23 22:50:26 +0900

    UnitTest1にトランザクション追加/staticイニシャライザに変更

--- Test_TodoApp/UnitTest1.cs
+++ Test_TodoApp/UnitTest1.cs
@@ -6,23 +6,17 @@ namespace Test_TodoApp;
 
 public class UnitTest1
 {
-    private static AppDbContext db;
+    private static readonly AppDbContext db;
 
-    public static void Initialize()
+    static UnitTest1()
     {
-        if (db != null) { return; }
-
         var builder = new DbContextOptionsBuilder<AppDbContext>();
         builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoAppDb-test;Trusted_Connection=True;");
         db = new AppDbContext(builder.Options);
 
         db.Database.EnsureDeleted();
         db.Database.EnsureCreated();
-    }
 
-    public UnitTest1()
-    {
-        Initialize();
     }
 
     public static AppDbContext GetContext() => db;
@@ -30,21 +24,29 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        db.Add(TodoTask.Create("aa","sss"));
+        using var transaction = db.Database.BeginTransaction();
+
+        db.Add(TodoTask.Create("aa", "sss"));
         db.SaveChanges();
 
         var item = db.TodoTasks.First();
         Assert.Equal("aa", item.Title);
+
+        transaction.Rollback();
     }
 
 
     [Fact]
     public void Test2()
     {
-        db.Add(TodoTask.Create("bb","sss"));
+        using var transaction = db.Database.BeginTransaction();
+
+        db.Add(TodoTask.Create("bb", "sss"));
         db.SaveChanges();
 
         var item = db.TodoTasks.First();
         Assert.Equal("bb", item.Title);
+
+        transaction.Rollback();
     }
 }
